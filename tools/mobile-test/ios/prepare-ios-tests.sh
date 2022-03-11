b2a574ba1c5ffc5341cec9774412c099f6790129
Armando Garcia <agarcia17@icloud.com>
2022-03-11 11:41:32 -0800

    ios,test: add tests for ios platform

---
+++ tools/mobile-test/ios/prepare-ios-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+if [ "$DEVICE_ID" = "" ]; then
+  TARGET_DEVICE=""
+else
+  TARGET_DEVICE="-i $DEVICE_ID"
+fi
+
+SCRIPT_BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
+NODEJS_BASE_DIR="$( cd "$( dirname "$0" )" && cd .. && cd .. && cd .. && pwd )"
+TEST_PROXY_TARGETDIR="$( cd "$NODEJS_BASE_DIR" && mkdir -p ./out/ios.release/ && cd ./out/ios.release/ && pwd )"
+
+# Remove symbolic links, which might make the iOS application invalid to install
+set +e
+
+FILES_TO_DELETE=(
+  "$NODEJS_BASE_DIR/test/fixtures/wasi/subdir/input_link.txt"
+  "$NODEJS_BASE_DIR/test/fixtures/wasi/subdir/loop1"
+  "$NODEJS_BASE_DIR/test/fixtures/wasi/subdir/loop2"
+  "$NODEJS_BASE_DIR/test/fixtures/wasi/subdir/outside.txt"
+)
+
+for i in "${FILES_TO_DELETE[@]}"; do
+  if [ -f "$i" ]; then
+      unlink "$i"
+  fi
+done
+
+set -e
+
+xcodebuild build -project "$SCRIPT_BASE_DIR/testnode/testnode.xcodeproj" -target "testnode" -configuration Release -arch arm64 -sdk "iphoneos" SYMROOT="$TEST_PROXY_TARGETDIR"
+
+cp "$SCRIPT_BASE_DIR/node-ios-proxy.sh" "$TEST_PROXY_TARGETDIR/node"
+
+ios-deploy -t 120 --uninstall $TARGET_DEVICE -b "$TEST_PROXY_TARGETDIR/Release-iphoneos/testnode.app" -u --noninteractive --args "--copy-path-for-testing"
+
+exit ${PIPESTATUS[0]}
